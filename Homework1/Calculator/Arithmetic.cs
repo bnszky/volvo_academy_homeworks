33b17331fd9c08acb3da271a448f75952facda11
bnszky <michal.banaszkiewicz.pvt@gmail.com>
2023-11-26 13:26:14 +0100

    Move some functions into static class Arithmetic and ConsoleManager

---
+++ Homework1/Calculator/Arithmetic.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal static class Arithmetic
+    {
+        public static double AddTwoNumbers(double a, double b) { return a + b; }
+        public static double SubtractTwoNumbers(double a, double b) { return a - b; }
+        public static double MultiplyTwoNumbers(double a, double b) { return a * b; }
+        public static double? DivideTwoNumbers(double a, double b)
+        {
+            if (b == 0)
+            {
+                Console.WriteLine("You can't divide by zero!");
+                return null;
+            }
+            return a / b;
+        }
+        public static long? FactorialFromN(int n)
+        {
+            if (n < 0)
+            {
+                Console.WriteLine("Factorial of negative integer doesn't exist!");
+                return null;
+            }
+            if (n == 0) return 1;
+            return FactorialFromN(n - 1) * n;
+        }
+
+        // Very intuitive implementation of exponentation
+        private static double SlowerPower(double x, int n)
+        {
+            double power = 1;
+            while (n-- > 0)
+            {
+                power *= x;
+            }
+            return power;
+        }
+        // Smarter way of exponentation using divide and conquer technique
+        private static double SmarterPower(double x, int n)
+        {
+            if (n == 0) return 1;
+            double power = SmarterPower(x, n / 2);
+            power *= power;
+            if (n % 2 == 1) power *= x;
+            return power;
+        }
+        public static double? Power(double x, int n)
+        {
+            if (n < 0 && x == 0)
+            {
+                Console.WriteLine("0 to negative exponent is undefined!");
+                return null;
+            }
+
+            double result = SmarterPower(x, Math.Abs(n));
+            if (n < 0) return ((double)1 / result);
+            return result;
+        }
+    }
+}
---
+++ Homework1/Calculator/ConsoleManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal static class ConsoleManager
+    {
+        public static int? SelectOperation()
+        {
+            Console.WriteLine("Please select operation by typing corresponding number:");
+            Console.WriteLine("1. Addition");
+            Console.WriteLine("2. Subtraction");
+            Console.WriteLine("3. Multiplication");
+            Console.WriteLine("4. Division");
+            Console.WriteLine("5. Factorial");
+            Console.WriteLine("6. Exponentiation");
+            Console.WriteLine("7. Exit");
+            string input = Console.ReadLine();
+            int number;
+            return (int.TryParse(input, out number) && number >= 1 && number <= 7) ? number : null;
+        }
+
+        public static bool ReadTwoNumbers(out double a, out double b)
+        {
+            Console.WriteLine("Please type first number: ");
+            bool isADouble = double.TryParse(Console.ReadLine(), out a);
+            Console.WriteLine("Please type second number: ");
+            bool isBDouble = double.TryParse(Console.ReadLine(), out b);
+
+            if (!isADouble || !isBDouble)
+            {
+                Console.WriteLine("Your input is incorrect!");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
